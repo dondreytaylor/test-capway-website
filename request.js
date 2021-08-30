98ca317a45186b34c43c783ae70f84bd547dffcd
Jesse_Hines <jhinesgu99@gmail.com>
2021-08-30 14:32:05 -0400

    email onclick

--- request.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "https://gateway.dev.cpwys.co/api/v1/content/fetch/learnmoney"
-    }).then(function(data) {
-       $('.know-id').append(data.id);
-       $('.know-content').append(data.content);
-    });
-});
