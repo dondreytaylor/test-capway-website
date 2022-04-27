b991f86b6c1e36913742f0d6fb3ec18993b786c1
asbillings07 <superswiftdeveloper@gmail.com>
2022-04-27 18:18:56 -0400

    finish downloads section

--- src/components/download.js
+++ src/components/download.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 export const Download = () => {
+  // const uri = 'https://web.dev.cpwys.co/assets/module-site/pages/page-landing/'
+  // const createDownloadOptions = ['google-play.svg', 'app-store.svg'].map(option => {
+  //   return (
+  //     <div className="download"></div>
+  //   )
+  // })
+
   return (
-    <div id='download-container'>
-      <div className='download-inner'>
+    <div className='section-three'>
+      <div className='download-container'>
         <h2>Download CapWay and Start Your Money Account. </h2>
-        <div >
-          <div className="download" id='first'></div>
+        <div className='download-container__options'>
+          <div className="download" id='second'></div>
           <div className="download" id='second'></div>
         </div>
       </div>
